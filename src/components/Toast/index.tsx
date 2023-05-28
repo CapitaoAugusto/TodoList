909ef9514f14fe7f02ca8e6629e9e7d1d7fe854a
/Otávio Augusto <vedanajazz@gmail.com>
2023-05-28 17:36:12 -0300

    feature-TodoListDesktop
    Projeto TodoList com adaptabilidade/responsividade para dispositivos desktops.

---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,20 @@
+import styles from './index.module.css';
+import { useToast } from '../../hooks/useToast';
+
+
+type ToastPropos = {
+    message: string;
+    type: 'success' | 'danger';
+}
+
+
+export const Toast = ({ message, type }: ToastPropos) => {
+
+    const { isHidden } = useToast();
+
+    return (
+        <aside className={isHidden ? styles.container : styles.container_show}>
+            <p className={type == "success" ? styles.success : styles.danger}>{message}</p>
+        </aside>
+    )
+}
